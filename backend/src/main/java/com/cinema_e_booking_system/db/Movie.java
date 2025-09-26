e6a8f3434bd8d4c3dbddae70f35a9939f6dcf071
Jake Palanca <jakepalanca@protonmail.com>
2025-09-26 09:53:40 -0400

    Corrected getters and setters

--- backend/src/main/java/com/cinema_e_booking_system/db/Movie.java
+++ backend/src/main/java/com/cinema_e_booking_system/db/Movie.java
@@ -8,8 +8,8 @@ import jakarta.persistence.*;
 public class Movie {
     // TODO: Add remaining categories
     public enum MovieCategory {
-        SCI_FI, 
-        ROMANCE, 
+        SCI_FI,
+        ROMANCE,
         ACTION
     }
 
@@ -39,18 +39,19 @@ public class Movie {
 
     @Enumerated(EnumType.STRING)
     MPAA_rating mpaaRating;
-        public Movie() {
+
+    public Movie() {
     }
 
     public Movie(
-        String title,
-        MovieCategory movieCategory,
-        List<String> cast,
-        String director,
-        String producer,
-        String synopsis,
-        String trailerLink,
-        MPAA_rating mpaaRating
+            String title,
+            MovieCategory movieCategory,
+            List<String> cast,
+            String director,
+            String producer,
+            String synopsis,
+            String trailerLink,
+            MPAA_rating mpaaRating
     ) {
         this.title = title;
         this.movieCategory = movieCategory;
@@ -62,70 +63,69 @@ public class Movie {
         this.mpaaRating = mpaaRating;
     }
 
-    //Getters and Setters
     public String getTitle() {
-          return title;
+        return title;
     }
 
-    public void setTitle(String newTitle) {
-          title = newTitle;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public MovieCategory getMovieCategory() {
-            return movieCategory;
+        return movieCategory;
     }
 
-    public void setMovieCategory(MovieCategory newMovieCategory) {
-            return newMovieCategory.toString();
+    public void setMovieCategory(MovieCategory movieCategory) {
+        this.movieCategory = movieCategory;
     }
 
-  public List<String> getCast() {
-          return cast;
-  }
+    public List<String> getCast() {
+        return cast;
+    }
 
-  public void setCast(List<String> newCast) {
-          cast = newCast;
-  }
+    public void setCast(List<String> cast) {
+        this.cast = cast;
+    }
 
-  public String getDirector() {
-          return director;
-  }
+    public String getDirector() {
+        return director;
+    }
 
-  public void setDirector(String newDirector) {
-          director = newDirector;
-  }
+    public void setDirector(String newDirector) {
+        director = newDirector;
+    }
 
-  public String getProducer() {
-          return producer;
-  }
+    public String getProducer() {
+        return producer;
+    }
 
-  public void setProducer(String newProducer) {
-          producer = newProducer;
-  }
+    public void setProducer(String newProducer) {
+        producer = newProducer;
+    }
 
-  public String getSynopsis() {
-          return synopsis;
-  }
+    public String getSynopsis() {
+        return synopsis;
+    }
 
-  public void setSynopsis() {
-          synopsis = synopsis;
-  }
+    public void setSynopsis() {
+        synopsis = synopsis;
+    }
 
-  public String getTrailerLink() {
-    return trailerLink;
-  }
+    public String getTrailerLink() {
+        return trailerLink;
+    }
 
-  public void setTrailerLink(String trailerLink) {
-          trailerLink = trailerLink;
-  }
+    public void setTrailerLink(String trailerLink) {
+        trailerLink = trailerLink;
+    }
 
-  public MPAA_rating getMpaaRating() {
-          return mpaaRating;
-  }
+    public MPAA_rating getMpaaRating() {
+        return mpaaRating;
+    }
 
-  public setMpaaRating(MPAA_rating newMpaaRating) {
-          mpaaRating = newMpaaRating;
-  }
+    public void setMpaaRating(MPAA_rating newMpaaRating) {
+        mpaaRating = newMpaaRating;
+    }
 }
    
 

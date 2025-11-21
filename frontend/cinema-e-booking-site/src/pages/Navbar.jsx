acfeee5a3a9c92243fb982e439b7c0dad8a5bba4
jakepalanca <jake@protonmail.com>
2025-11-21 13:45:10 -0500

    Redesign navbar

--- frontend/cinema-e-booking-site/src/pages/Navbar.jsx
+++ frontend/cinema-e-booking-site/src/pages/Navbar.jsx
@@ -14,49 +14,45 @@ function Navbar() {
     };
 
     return (
-        <header>
-            <nav>
-                <ul className="flex-list">
-                    <div className="logo">
-                        <li>
-                            <a href="/">
-                                <img src="logo.png" alt="Team 14"/>
-                            </a>
-                        </li>
-                    </div>
+        <header className="navbar-header">
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="logo-link">
+                        <img src="/logo.png" alt="Cinema Logo" className="navbar-logo"/>
+                    </Link>
+                    <ul className="nav-list">
+                        <li><Link to="/showtimes" className="nav-item">Showtimes</Link></li>
+                        <li><Link to="/promotions" className="nav-item">Promotions</Link></li>
+                        <li><Link to="/contact" className="nav-item">Contact Us</Link></li>
+                        <li><Link to="/browse" className="nav-item">Browse</Link></li>
 
-                    <li><Link to="/showtimes" className="nav-item">Showtimes</Link></li>
-                    <li><Link to="/promotions" className="nav-item">Promotions</Link></li>
-                    <li><Link to="/contact" className="nav-item">Contact Us</Link></li>
-                    <li><Link to="/browse" className="nav-item">Browse</Link></li>
-
-                    {isAuthenticated ? (
-                        <>
-                            {isAdmin && (
-                                <li><Link to="/admin-homepage" className="nav-item">Admin</Link></li>
-                            )}
-                            {!isAdmin && (
-                                <li><Link to="/edit-profile" className="nav-item">Edit Profile</Link></li>
-                            )}
-                            <li>
-                                <Link
-                                    to="/"
-                                    className="nav-item"
-                                    onClick={(e) => { e.preventDefault(); handleLogout(); }}
-                                >
-                                    Log Out
-                                </Link>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li><Link to="/register" className="nav-item">Sign Up</Link></li>
-                            <li><Link to="/login" className="nav-item">Sign In</Link></li>
-                        </>
-                    )}
-                </ul>
+                        {isAuthenticated ? (
+                            <>
+                                {isAdmin && (
+                                    <li><Link to="/admin-homepage" className="nav-item">Admin</Link></li>
+                                )}
+                                {!isAdmin && (
+                                    <li><Link to="/edit-profile" className="nav-item">Edit Profile</Link></li>
+                                )}
+                                <li>
+                                    <Link
+                                        to="/"
+                                        className="nav-item"
+                                        onClick={(e) => { e.preventDefault(); handleLogout(); }}
+                                    >
+                                        Log Out
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li><Link to="/register" className="nav-item">Sign Up</Link></li>
+                                <li><Link to="/login" className="nav-item">Sign In</Link></li>
+                            </>
+                        )}
+                    </ul>
+                </div>
             </nav>
-            <hr />
         </header>
     );
 }
